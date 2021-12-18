47db374e08c4d42b979f021256d15fb74c3fc685
ehlera1 <adam.p.ehlers@gmail.com>
2021-12-18 15:56:45 -0600

    13.5.6 Complete

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,10 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
-// // Pass our map layers into our layers control and add the layers control to the map.
-// L.control.layers(baseMaps).addTo(map);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -29,34 +25,33 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [43.7, -79.3],
   zoom: 11,
-  layers: [satellitestreets]
+  layers: [streets]
 });
 
 
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/ehlera1/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/static/data/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/ehlera1/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/static/data/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  color: "blue",
+  fillColor: "#ffffa1",
+  weight: 1
 }
+
 
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function(data) {
   console.log(data);
-
-
-
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addto(map);
-// L.geoJSON(data, {
-//   style:myStyle, 
-//   onEachFeature: function(feature, lawyer) {
-//     lawyer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
-//   }
-// })
-// .addTo(map);
-});
+L.geoJSON(data, {
+  style:myStyle, 
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Neighboorhood: " + feature.properties.AREA_NAME + "</h3>");
+  }
+})
+.addTo(map);
+});;
+
